02dafad92a4167e34576c4f67396abfa9a8e2fc8
Greg Mall <gregmall157@gmail.com>
2020-06-25 17:31:26 -0700

    more testing

--- test/example.test.js
+++ test/example.test.js
@@ -19,27 +19,27 @@ test('it should return 5 when passed 2 and 3', (expect) => {
     // Make assertions about what is expected versus the actual result
     
 });
-test('it should return a 5 when passed a 12 and 7', (expect)=>{
-    const argument1 = 12;
+test('it should return a 9 when passed a 16 and 7', (expect)=>{
+    const argument1 = 16;
     const argument2 =7;
-    const expected = 5;
+    const expected = 9;
     const actual= subtract(argument1, argument2);
     expect.equal(actual,expected);
 
 });
-test('it should return a 8 when passed a 2 and 4', (expect)=>{
-    const argument1 = 2;
+test('it should return a 12 when passed a 3 and 4', (expect)=>{
+    const argument1 = 4;
     const argument2 =4;
-    const expected = 8;
+    const expected = 16;
     const actual =product(argument1, argument2);
     expect.equal(actual,expected);
 
 });
 
-test('it should return a 2 when passed a 12 and 6', (expect)=>{
-    const argument1 = 12;
-    const argument2 =6;
-    const expected = 2;
+test('it should return a 8 when passed a 16 and 2', (expect)=>{
+    const argument1 = 16;
+    const argument2 =2;
+    const expected = 8;
     const actual =divide(argument1, argument2);
     expect.equal(actual,expected);
 
